674d2071f75097309796ceec6da0b8e73d7be341
Tobias Boogh <t@boogh.se>
2021-02-20 20:51:36 +0100

    Initial Commit

---
+++ Reduxulous/Reduxulous.h
@@ -0,0 +1,18 @@
+//
+//  Reduxulous.h
+//  Reduxulous
+//
+//  Created by Tobias Boogh on 2021-02-20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Reduxulous.
+FOUNDATION_EXPORT double ReduxulousVersionNumber;
+
+//! Project version string for Reduxulous.
+FOUNDATION_EXPORT const unsigned char ReduxulousVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Reduxulous/PublicHeader.h>
+
+
